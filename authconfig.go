41b9d0f55c8aa9a1494c4c682ab8332e2ab88ead
Shounak Chandra <69243372+shounakdev@users.noreply.github.com>
2025-08-03 21:36:31 +0530

    Update authconfig.go

--- authconfig.go
+++ authconfig.go
@@ -7,8 +7,8 @@ import (
 
 // Central place for your Google OAuth credentials
 const (
-	CLIENT_ID     = "769146177228-h94nrtlne4cgd1351fcf1ld5sfekjduv.apps.googleusercontent.com"
-	CLIENT_SECRET = "YGOCSPX-rAvQoD8YfA65Qkp2gSKZLgtXBuQJ"
+	CLIENT_ID     = ""
+	CLIENT_SECRET = ""
 	REDIRECT_URI  = "http://localhost:8080/oauth2callback"
 )
 
